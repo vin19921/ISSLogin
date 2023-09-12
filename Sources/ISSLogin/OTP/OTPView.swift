be1b648872bd415974f4cc09d582baef16ec933e
Wing Seng <chewwingseng@gmail.com>
2023-09-12 11:49:09 +0800

    Update OTPView.swift

--- Sources/ISSLogin/OTP/OTPView.swift
+++ Sources/ISSLogin/OTP/OTPView.swift
@@ -7,8 +7,10 @@
 
 import ISSTheme
 import SwiftUI
+import Combine
 
 public struct OTPView: View {
+
     @ObservedObject private var presenter: OTPPresenter
     
     @State private var emailText = ""
@@ -22,94 +24,127 @@ public struct OTPView: View {
         self.presenter = presenter
     }
 
-    @State private var otp: String = ""
-    
-    public var body: some View {
-        VStack {
-            Text("Enter OTP")
-                .font(.largeTitle)
-                .padding()
-            
-            HStack(spacing: 20) {
-                ForEach(0..<6, id: \.self) { index in
-                    OTPDigitTextField(value: digitAtIndex(index), isLastField: index == 5) {
-                        if index < 5 {
-                            let nextIndex = otp.index(otp.startIndex, offsetBy: index + 1)
-                            let nextDigit = String(otp[nextIndex])
-                            if nextDigit.isEmpty {
-                                // Set focus to the next digit field
-                                otp.replaceSubrange(nextIndex...nextIndex, with: "0") // Place a placeholder digit
-                            }
-                        }
-                    }
-                }
-            }
-            .padding()
-            
-            Button(action: {
-                // Implement OTP verification logic here
-                // You can compare the entered OTP (otp) with the expected OTP
-                // and display an alert or navigate to the next screen based on the result
-                print("Entered OTP: \(otp)")
-            }) {
-                Text("Verify OTP")
-                    .font(.headline)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-            .padding()
-            
-            Spacer()
-        }
-        .background(Theme.current.grayDisabled.color)
+    //MARK -> PROPERTIES
+
+    enum FocusPin {
+        case  pinOne, pinTwo, pinThree, pinFour
     }
-    
-    func digitAtIndex(_ index: Int) -> Binding<String> {
-        return Binding<String>(
-            get: {
-                if index < otp.count {
-                    let currentIndex = otp.index(otp.startIndex, offsetBy: index)
-                    return String(otp[currentIndex])
-                } else {
-                    return ""
-                }
-            },
-            set: { newValue in
-                if newValue.count > 0 {
-                    if index < otp.count {
-                        let currentIndex = otp.index(otp.startIndex, offsetBy: index)
-                        otp.replaceSubrange(currentIndex...currentIndex, with: newValue)
-                        if index < 5 {
-                            let nextIndex = otp.index(otp.startIndex, offsetBy: index + 1)
-                            DispatchQueue.main.async {
-                                UIApplication.shared.sendAction(#selector(UIResponder.becomeFirstResponder), to: nil, from: nil, for: nil)
+
+    @FocusState private var pinFocusState : FocusPin?
+    @State var pinOne: String = ""
+    @State var pinTwo: String = ""
+    @State var pinThree: String = ""
+    @State var pinFour: String = ""
+
+    //MARK -> BODY
+    public var body: some View {
+            VStack {
+
+                Text("Verify your Email Address")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+
+
+                Text("Enter 4 digit code we'll text you on Email")
+                    .font(.caption)
+                    .fontWeight(.thin)
+                    .padding(.top)
+
+                HStack(spacing:15, content: {
+
+                    TextField("", text: $pinOne)
+                        .modifier(OtpModifer(pin:$pinOne))
+                        .onChange(of:pinOne){newVal in
+                            if (newVal.count == 1) {
+                                pinFocusState = .pinTwo
                             }
                         }
-                    }
-                }
+                        .focused($pinFocusState, equals: .pinOne)
+
+                    TextField("", text:  $pinTwo)
+                        .modifier(OtpModifer(pin:$pinTwo))
+                        .onChange(of:pinTwo){newVal in
+                            if (newVal.count == 1) {
+                                pinFocusState = .pinThree
+                            }else {
+                                if (newVal.count == 0) {
+                                    pinFocusState = .pinOne
+                                }
+                            }
+                        }
+                        .focused($pinFocusState, equals: .pinTwo)
+
+
+                    TextField("", text:$pinThree)
+                        .modifier(OtpModifer(pin:$pinThree))
+                        .onChange(of:pinThree){newVal in
+                            if (newVal.count == 1) {
+                                pinFocusState = .pinFour
+                            }else {
+                                if (newVal.count == 0) {
+                                    pinFocusState = .pinTwo
+                                }
+                            }
+                        }
+                        .focused($pinFocusState, equals: .pinThree)
+
+
+                    TextField("", text:$pinFour)
+                        .modifier(OtpModifer(pin:$pinFour))
+                        .onChange(of:pinFour){newVal in
+                            if (newVal.count == 0) {
+                                pinFocusState = .pinThree
+                            }
+                        }
+                        .focused($pinFocusState, equals: .pinFour)
+
+
+                })
+                .padding(.vertical)
+
+                Button(action: {
+                    print("\(pinOne)\(pinTwo)\(pinThree)\(pinFour)")
+                }, label: {
+                    Spacer()
+                    Text("Veify")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Spacer()
+                })
+                .padding(15)
+                .background(Color.blue)
+                .clipShape(Capsule())
+                .padding()
             }
-        )
+
     }
 }
 
-struct OTPDigitTextField: View {
-    @Binding var value: String
-    var isLastField: Bool = false
-    var nextResponder: (() -> Void)? = nil
-    
-    var body: some View {
-        TextField("", text: $value)
-            .frame(width: 40, height: 40)
-            .font(.largeTitle)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
+struct OtpModifer: ViewModifier {
+
+    @Binding var pin : String
+
+    var textLimt = 1
+
+    func limitText(_ upper : Int) {
+        if pin.count > upper {
+            self.pin = String(pin.prefix(upper))
+        }
+    }
+
+
+    //MARK -> BODY
+    func body(content: Content) -> some View {
+        content
             .multilineTextAlignment(.center)
             .keyboardType(.numberPad)
-            .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { _ in
-                if self.isLastField {
-                    self.nextResponder?()
-                }
-            }
+            .onReceive(Just(pin)) {_ in limitText(textLimt)}
+            .frame(width: 45, height: 45)
+            .background(Color.white.cornerRadius(5))
+            .background(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color("blueColor"), lineWidth: 2)
+            )
     }
 }

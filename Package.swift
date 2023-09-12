029816f7c35369b79b426585e592ee1633eb36d5
Wing Seng <chewwingseng@gmail.com>
2023-09-12 10:00:16 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,13 +16,16 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/iSoftStoneMY/iSSBooking-iOS-Theme", branch: "develop"),
+        .package(url: "https://github.com/iSoftStoneMY/iSSBooking-iOS-CommonUI", branch: "develop"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ISSLogin",
-            dependencies: [.product(name: "ISSTheme", package: "iSSBooking-iOS-Theme")]),
+            dependencies: [.product(name: "ISSTheme", package: "iSSBooking-iOS-Theme"),
+                           .product(name: "ISSCommonUI", package: "iSSBooking-iOS-CommonUI"),
+            ]),
         .testTarget(
             name: "ISSLoginTests",
             dependencies: ["ISSLogin"]),

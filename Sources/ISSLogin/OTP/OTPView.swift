99f2e6799cffff75878edba76aa5f743461a8d19
Wing Seng <chewwingseng@gmail.com>
2023-09-14 18:00:32 +0800

    Update OTPView.swift

--- Sources/ISSLogin/OTP/OTPView.swift
+++ Sources/ISSLogin/OTP/OTPView.swift
@@ -9,6 +9,7 @@ import Combine
 import ISSCommonUI
 import ISSTheme
 import SwiftUI
+import UIKit
 
 public struct OTPView: View {
 
@@ -47,11 +48,11 @@ public struct OTPView: View {
                                             verticalPadding: 0)
                     HStack(spacing: 0) {
                         Spacer()
-                        CTextField(text: $pinText, placeholder: "")
+                        OTPTextField(otp: $pinText, maxLength: 6, symbolWidth: 16, font: Theme.current.headline4.uiFont)
                             .lineLimit(1)
-                            .fontWithLineHeight(font: Theme.current.headline4.uiFont,
-                                                lineHeight: Theme.current.headline4.lineHeight,
-                                                verticalPadding: 0)
+//                            .fontWithLineHeight(font: Theme.current.headline4.uiFont,
+//                                                lineHeight: Theme.current.headline4.lineHeight,
+//                                                verticalPadding: 0)
                             .background(Theme.current.backgroundGray.color)
                             .textContentType(.oneTimeCode)
                             .onChange(of: pinText, perform: {
@@ -206,24 +207,71 @@ struct OtpModifer: ViewModifier {
     }
 }
 
-struct CTextField: View {
-    @Binding var text: String
-    var placeholder: String
+struct OTPTextField: UIViewRepresentable {
+    @Binding var otp: String
+    var maxLength: Int = 6
+    var symbolWidth: CGFloat = 16
+    var font: UIFont = .systemFont(ofSize: 30)
 
-    var body: some View {
-        VStack {
-            if text.isEmpty {
-                Text(placeholder)
-                    .foregroundColor(.gray)
-                    .tracking(16) // Apply character spacing to the placeholder text
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField()
+        textField.delegate = context.coordinator
+        textField.font = font
+        textField.keyboardType = .numberPad
+        return textField
+    }
+
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = otp
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: OTPTextField
+
+        init(_ parent: OTPTextField) {
+            self.parent = parent
+        }
+
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            let maxLength = parent.maxLength
+            let symbolWidth = parent.symbolWidth
+            let font = parent.font
+
+            if string == "" {
+                return true
             }
-            TextField("", text: $text)
-//                .font(.system(size: 18))
-//                .padding(10)
-//                .background(
-//                    RoundedRectangle(cornerRadius: 10)
-//                        .stroke(Color.blue, lineWidth: 1)
-//                )
+
+            if textField.text!.count + string.count - range.length > maxLength {
+                return false
+            }
+
+            let currentText = NSMutableAttributedString(attributedString: NSAttributedString(string: textField.text ?? ""))
+            currentText.deleteCharacters(in: range)
+            var newStringLength = 0
+
+            for char in string {
+                let newSymbol = NSMutableAttributedString(string: String(char))
+                newSymbol.addAttribute(.font, value: font, range: NSMakeRange(0, 1))
+                let currentSymbolWidth = newSymbol.size().width
+                let kern = symbolWidth - currentSymbolWidth
+                newSymbol.addAttribute(.kern, value: kern, range: NSMakeRange(0, 1))
+
+                currentText.insert(newSymbol, at: range.location + newStringLength)
+                newStringLength += 1
+            }
+
+            if currentText.length == maxLength {
+                currentText.addAttribute(.kern, value: 0, range: NSMakeRange(maxLength - 1, 1))
+            }
+
+            textField.attributedText = currentText
+            parent.otp = currentText.string
+
+            return false
         }
     }
 }

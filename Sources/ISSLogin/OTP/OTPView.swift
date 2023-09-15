dae43238e0a972b65209ade097a17aa96c7d5adb
Wing Seng <chewwingseng@gmail.com>
2023-09-15 11:25:51 +0800

    Update OTPView.swift

--- Sources/ISSLogin/OTP/OTPView.swift
+++ Sources/ISSLogin/OTP/OTPView.swift
@@ -85,7 +85,7 @@ public struct OTPView: View {
                                         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
                                     }
                                 })
-                                .frame(width: 200, height: 28)
+//                                .frame(width: 200, height: 28)
                                 .keyboardType(.numberPad)
                                 .accentColor(Color.black)
 //                                .multilineTextAlignment(.center)
@@ -252,16 +252,15 @@ struct OTPTextField: UIViewRepresentable {
             currentText.deleteCharacters(in: range)
             var newStringLength = 0
 
-            for (index, char) in string.enumerated() {
-                let isNotLastCharacter = index != string.count - 1
+            for char in string {
                 let newSymbol = NSMutableAttributedString(string: String(char))
                 newSymbol.addAttribute(.font, value: font, range: NSMakeRange(0, 1))
                 let currentSymbolWidth = newSymbol.size().width
                 let kern = symbolWidth - currentSymbolWidth
-                
-                if isNotLastCharacter {
+
+                if string.count < maxLength {
                     newSymbol.addAttribute(.kern, value: kern, range: NSMakeRange(0, 1))
-                    print("not isLastCharacter")
+                    print("added kern \(string.count)")
                 }
 
                 currentText.insert(newSymbol, at: range.location + newStringLength)

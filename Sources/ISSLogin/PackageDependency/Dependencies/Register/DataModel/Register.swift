7b6d23c0da14bf0b376c0cd373b2c41e0d421f3a
Wing Seng <chewwingseng@gmail.com>
2023-09-20 11:48:57 +0800

    update

---
+++ Sources/ISSLogin/PackageDependency/Dependencies/Register/DataModel/Register.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by Wing Seng Chew on 20/09/2023.
+//
+
+public struct Register: Codable {
+    public let id: String?
+    public let name: String?
+    public let email: String?
+    public let mobileNo: Int?
+    public let isDraft: Int16?
+    public let isCompleteRegister: Int16?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case email
+        case mobileNo
+        case isDraft
+        case isCompleteRegister
+    }
+}
+
+public struct RegisterResponse: Codable {
+    public let resultCode: Int16
+    public let resultMessage: String
+    public let data: Register
+}
+
+public extension RegisterResponse {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        resultCode = try container.decode(Int16.self, forKey: .resultCode)
+        resultMessage = try container.decode(String.self, forKey: .resultMessage)
+        data = try container.decode(Register.self, forKey: .data)
+    }
+}

89619087a51184c4a161ab53ddb6e7b4ef47dd34
Wing Seng <chewwingseng@gmail.com>
2023-09-20 10:29:30 +0800

    Create OTP.swift

---
+++ Sources/ISSLogin/PackageDependency/Dependencies/OTP/DataModel/OTP.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  
+//
+//  Created by Wing Seng Chew on 20/09/2023.
+//
+
+public struct OTP: Codable {
+    public let id: String?
+    public let email: String?
+    public let mobileNo: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case email
+        case mobileNo
+    }
+}
+
+public struct OTPResponse: Codable {
+    public let resultCode: Int16
+    public let resultMessage: String
+    public let data: OTP
+}
+
+public extension OTPResponse {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        resultCode = try container.decode(Int16.self, forKey: .resultCode)
+        resultMessage = try container.decode(String.self, forKey: .resultMessage)
+        data = try container.decode(OTP.self, forKey: .data)
+    }
+}

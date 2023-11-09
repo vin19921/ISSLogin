0a3297941c54dbd727a13d2cf2b6f8ffaeee0bb8
Wing Seng <chewwingseng@gmail.com>
2023-11-09 15:03:49 +0800

    update

---
+++ Sources/ISSLogin/Scene/Register/RegisterConstants.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Wing Seng Chew on 09/11/2023.
+//
+
+import Foundation
+
+public enum RegisterImageAssets: String, ImageLoader, ImageLoaderSUI {
+    case ic_email
+}
--- Sources/ISSLogin/Scene/Register/RegisterView.swift
+++ Sources/ISSLogin/Scene/Register/RegisterView.swift
@@ -128,7 +128,7 @@ public struct RegisterView: View {
                                                                    validateText: "Please enter valid email",
                                                                    regEx: RegExConstants.emailRegEx,
                                                                    isRequiredText: "Please enter valid email",
-                                                                   leadingImageIcon: Image("ic_email"))
+                                                                   leadingImageIcon: RegisterImageAssets.ic_email.image)
                 )
                 
                 Button(action: {

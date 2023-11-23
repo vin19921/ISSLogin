0d0b75669721716dfc09c7851eb2c01a1d0781fe
Wing Seng <chewwingseng@gmail.com>
2023-11-23 14:49:32 +0800

    Update LoginDataProviderLogic.swift

--- Sources/ISSLogin/PackageDependency/Dependencies/Login/LoginDataProviderLogic.swift
+++ Sources/ISSLogin/PackageDependency/Dependencies/Login/LoginDataProviderLogic.swift
@@ -10,7 +10,7 @@ import Combine
 /// The provider facilitate communication between the controller from the UI layer and the services that are used to retrieve/persist data.
 /// This helps provide proper separation of concerns between the these objects.
 public protocol LoginDataProviderLogic {
-    func fetchLogin(request: Login.Model.Request) -> AnyPublisher<LoginResponseDataModel, Error>
+    func fetchLogin(request: Login.Model.Request) -> AnyPublisher<LoginResponse, Error>
 //    func getUserInfo(key: UserInfoKey) -> String
     func saveUserInfo(loginDataModel: LoginDataModel)
 }

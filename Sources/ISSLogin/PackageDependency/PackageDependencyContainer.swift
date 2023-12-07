77898caeffcfbd0d8bc78486054ac59911ea9625
Wing Seng <chewwingseng@gmail.com>
2023-12-07 11:45:39 +0800

    Update PackageDependencyContainer.swift

--- Sources/ISSLogin/PackageDependency/PackageDependencyContainer.swift
+++ Sources/ISSLogin/PackageDependency/PackageDependencyContainer.swift
@@ -10,7 +10,7 @@ import ISSNetwork
 /// All dependencies that are required to be initialized once and passed down to the providers should be held here.
 public struct PackageDependencyContainer {
     public var networkMonitor: NetworkMonitor?
-    public var tabBarController: TabBarControlling?
+    public var tabBarController: TabBarControllerLogic?
 }
 
 public enum PackageDependency {

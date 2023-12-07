5f022e867d62473725ed2c803b7ebbf5784d5353
Wing Seng <chewwingseng@gmail.com>
2023-12-07 11:33:29 +0800

    update

---
+++ Sources/ISSLogin/PackageDependency/Dependencies/Profile/TabBarControllerLogic.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Wing Seng Chew on 07/12/2023.
+//
+
+public protocol TabBarControllerLogic {
+    func showMainTabBar()
+    func hideMainTabBar()
+}
--- Sources/ISSLogin/PackageDependency/ISSGateway+Extension.swift
+++ Sources/ISSLogin/PackageDependency/ISSGateway+Extension.swift
@@ -5,6 +5,7 @@
 //  Created by Wing Seng Chew on 07/12/2023.
 //
 
+import ISSNetwork
 import Foundation
 
 extension ISSGateway {

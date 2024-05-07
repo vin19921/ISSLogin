8c5324feb707253c6cb3b8912d5f08776641db1e
Wing Seng <chewwingseng@gmail.com>
2024-05-07 14:29:36 +0800

    Update TimeFrameDataModel.swift

--- Sources/ISSLogin/PackageDependency/Dependencies/TimeFrame/DataModel/TimeFrameDataModel.swift
+++ Sources/ISSLogin/PackageDependency/Dependencies/TimeFrame/DataModel/TimeFrameDataModel.swift
@@ -11,3 +11,38 @@ public protocol TimeFrameListResponseDataModel {
     var status: Int16 { get }
 }
 
+public struct TimeFrameListResponseDataModel: Codable {
+    public let resultCode: Int16
+    public let resultMessage: String
+    public let status: Int16
+
+    enum CodingKeys: String, CodingKey {
+        case resultCode
+        case resultMessage
+        case status
+    }
+}
+
+public extension TimeFrameListResponseDataModel {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        resultCode = try container.decodeIfPresent(Int16.self, forKey: .resultCode) ?? 0
+        resultMessage = try container.decodeIfPresent(String.self, forKey: .resultMessage) ?? ""
+        status = try container.decodeIfPresent(Int16.self, forKey: .status) ?? 0
+    }
+}
+
+public struct TimeFrameListResponse: Codable {
+    public let resultCode: Int16?
+    public let resultMessage: String?
+    public let status: Int16?
+}
+
+public extension TimeFrameListResponse {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        resultCode = try container.decode(Int16.self, forKey: .resultCode)
+        resultMessage = try container.decode(String.self, forKey: .resultMessage)
+        status = try container.decode(Int16.self, forKey: .status)
+    }
+}

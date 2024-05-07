a5056cd8a197c779114b1b0e47484a8e43e98cf6
Wing Seng <chewwingseng@gmail.com>
2024-05-07 14:31:37 +0800

    Update TimeFrameDataModel.swift

--- Sources/ISSLogin/PackageDependency/Dependencies/TimeFrame/DataModel/TimeFrameDataModel.swift
+++ Sources/ISSLogin/PackageDependency/Dependencies/TimeFrame/DataModel/TimeFrameDataModel.swift
@@ -5,12 +5,6 @@
 //  Created by Wing Seng Chew on 07/05/2024.
 //
 
-public protocol TimeFrameListResponseDataModel {
-    var resultCode: Int16 { get }
-    var resultMessage: String { get }
-    var status: Int16 { get }
-}
-
 public struct TimeFrameListResponseDataModel: Codable {
     public let resultCode: Int16
     public let resultMessage: String

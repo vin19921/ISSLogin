b8c49e7360f193bc3160f2d7686c34a0fef7af50
Wing Seng <chewwingseng@gmail.com>
2024-06-05 14:11:54 +0800

    Update MyAvailabilityListView.swift

--- Sources/ISSLogin/Scene/MyAvailability/MyAvailabilityListView.swift
+++ Sources/ISSLogin/Scene/MyAvailability/MyAvailabilityListView.swift
@@ -39,59 +39,58 @@ public struct MyAvailabilityListView: View {
                         }
                     Spacer()
                 case .success:
-                    
-                        VStack(spacing: 16) {
-                            VStack(spacing: 4) {
-                                HStack {
-                                    Text("Select day and time preferences")
-                                        .fontWithLineHeight(font: Theme.current.bodyOneBold.uiFont,
-                                                            lineHeight: Theme.current.bodyOneBold.lineHeight,
-                                                            verticalPadding: 0)
-                                    Spacer()
-                                }
-                                HStack {
-                                    Text("Specify your availability to ensure it aligns with the task requirements and your schedule.")
-                                        .fontWithLineHeight(font: Theme.current.bodyTwoMedium.uiFont,
-                                                            lineHeight: Theme.current.bodyTwoMedium.lineHeight,
-                                                            verticalPadding: 0)
-                                    Spacer()
-                                }
+                    VStack(spacing: 16) {
+                        VStack(spacing: 4) {
+                            HStack {
+                                Text("Select day and time preferences")
+                                    .fontWithLineHeight(font: Theme.current.bodyOneBold.uiFont,
+                                                        lineHeight: Theme.current.bodyOneBold.lineHeight,
+                                                        verticalPadding: 0)
+                                Spacer()
                             }
-                            ScrollView {
-                                VStack(spacing: 4) {
-                                    Spacer()
-                                }
-                            }
-
-                            Spacer()
-                            Button(action: {
-                                print("add new")
-                                presenter.routeToTimeFrame()
-                            }) {
-                                HStack {
-                                    Spacer()
-                                    LoginImageAssets.plusCircle.image
-                                        .resizable()
-//                                                .renderingMode(.template)
-                                        .frame(width: 24, height: 24)
-                                        .aspectRatio(contentMode: .fit)
-                                        .foregroundColor(Color.white)
-                                        .padding(.vertical, 12)
-
-                                    Text("Add New")
-                                        .fontWithLineHeight(font: Theme.current.bodyTwoBold.uiFont,
-                                                            lineHeight: Theme.current.bodyTwoBold.lineHeight,
-                                                            verticalPadding: 8)
-                                        .foregroundColor(Color.white)
-                                    Spacer()
-                                }
-                                .background(Color.black)
-                                .cornerRadius(25)
+                            HStack {
+                                Text("Specify your availability to ensure it aligns with the task requirements and your schedule.")
+                                    .fontWithLineHeight(font: Theme.current.bodyTwoMedium.uiFont,
+                                                        lineHeight: Theme.current.bodyTwoMedium.lineHeight,
+                                                        verticalPadding: 0)
+                                Spacer()
                             }
                         }
-                        .frame(height: UIScreen.main.bounds.height)
-                        .padding(.vertical)
-                        .padding(.horizontal, 24)
+                        ScrollView {
+                            VStack(spacing: 4) {
+                                HStack {
+                                }
+                                .frame(height: 123, width: UIScreen.main.bounds.width - 48)
+                            }
+                        }
+                        Spacer()
+                        Button(action: {
+                            print("add new")
+                            presenter.routeToTimeFrame()
+                        }) {
+                            HStack {
+                                Spacer()
+                                LoginImageAssets.plusCircle.image
+                                    .resizable()
+//                                                .renderingMode(.template)
+                                    .frame(width: 24, height: 24)
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(Color.white)
+                                    .padding(.vertical, 12)
+
+                                Text("Add New")
+                                    .fontWithLineHeight(font: Theme.current.bodyTwoBold.uiFont,
+                                                        lineHeight: Theme.current.bodyTwoBold.lineHeight,
+                                                        verticalPadding: 8)
+                                    .foregroundColor(Color.white)
+                                Spacer()
+                            }
+                            .background(Color.black)
+                            .cornerRadius(25)
+                        }
+                    }
+                    .padding(.vertical)
+                    .padding(.horizontal, 24)
                     
                 case let .failure(type):
                     Text("Error")
